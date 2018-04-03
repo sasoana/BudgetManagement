d7db12b661be5e262e35a9a9f9b0a8fa08c77996
soie2011 <sas.oana.2013@gmail.com>
2018-04-03 15:12:41 +0300

    changed TC

--- src/test/java/controller/MemberControllerTest.java
+++ src/test/java/controller/MemberControllerTest.java
@@ -25,7 +25,7 @@ public class MemberControllerTest {
     @Test
     public void testAddMemberValidName() throws Exception {
         memberController.setMembers(new ArrayList<Member>());
-        String name = "Di";
+        String name = "D";
         Assert.assertTrue(memberController.addMember(name));
         Assert.assertEquals(1, memberController.getMembers().size());
     }
